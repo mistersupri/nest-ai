5f13595e30d0af737e597c25fdd12f4fb02a6099
Ahmad Supriyanto <ahmad.supriyanto@marinetechs.co>
2023-08-06 14:55:15 +0700

    feat: use .env, add dotenv package, use elevenlabs voice, and refactor code

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from pydub import AudioSegment
 from pydub.playback import play
 import pyautogui
 import openai
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, LocalAgent
+from transformers import pipeline
+from elevenlabs import generate, play as elPlay, set_api_key
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+set_api_key(os.getenv("ELEVEN_API_KEY"))
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 CHUNK = 1024  # The number of frames per buffer
 FORMAT = pyaudio.paInt16  # The audio format (16-bit integer)
@@ -20,10 +31,41 @@ SILENCE_HOTWORD_INTERVAL = 1
 SILENCE_EVAL_INTERVAL = 3
 
 HOTWORD = "joni"
+AGENT_VOICE_FILENAME = "output.mp3"
+USER_VOICE_FILENAME = "recorded_audio.wav"
+LANGUAGE = "id-ID"
 
 is_eval_mode = False
 
-openai.api_key_path = "./.api_key"
+
+templates_obj = {
+    "basic": """
+            Kamu adalah seorang yang sangat pintar dari semua pelajaran.
+            Jawablah semua pertanyaan dari user.
+            Tidak perlu menjelaskan secara spesifik dan jawab secukupnya.
+            Tidak perlu selalu mengucapkan terima kasih,
+            tapi tanyakan apakah user ingin bertanya lagi.
+            Jika user tidak ingin bertanya lagi, baru ucapkan 'terima kasih ya!'
+            """,
+    "write": """
+            Kamu akan mengetikkan sesuatu pada komputer user. 
+            Jadi, pastikan jawaban kamu sangat singkat dan tidak perlu penjelasan apa pun.
+            Misal, user ingin kamu menuliskan topi dalam bahasa inggris,
+            maka kamu akan menjawab 'hat' tanpa ada tambahan jawaban lain.
+            """
+}
+
+
+def translate(text, language=LANGUAGE):
+    if (language == "id-ID"):
+        return text
+    words = {
+        "en-US": {
+            "terima kasih": "thank you",
+            "tulis": "write"
+        }
+    }
+    return words[language][text]
 
 
 def type_text(text, interval=0.1):
@@ -31,7 +73,7 @@ def type_text(text, interval=0.1):
     pyautogui.write(text, interval=interval)
 
 
-def text_to_audio(text, audio_filename, lang='id'):
+def text_to_audio(text, audio_filename, lang=LANGUAGE[:2]):
     tts = gTTS(text=text, lang=lang, slow=False)
     tts.save(audio_filename)
 
@@ -41,51 +83,72 @@ def play_audio(audio_filename):
     play(audio)
 
 
-def evaluate_command(text, temperature=0):
+def play_audio_from_text_google(text):
+    text_to_audio(text, AGENT_VOICE_FILENAME)
+    play_audio(AGENT_VOICE_FILENAME)
+
+
+def play_audio_from_text(text):
+    audio = generate(
+        text=text,
+        voice="Bella",
+        model='eleven_multilingual_v1'
+    )
+    elPlay(audio)
+
+
+# checkpoint = "tiiuae/falcon-7b"
+# model = AutoModelForCausalLM.from_pretrained(
+#     checkpoint, device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
+# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+# def ask_llm(text):
+#     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+#     generated_text = pipe(text, max_length=50, do_sample=False,
+#                           no_repeat_ngram_size=2)[0]
+#     return generated_text['generated_text']
+
+
+def ask_gpt(text, template="basic"):
     gpt_mode = "gpt-3.5-turbo"
-    output_filename = "output.mp3"
-    if ("tulis" in text):
-        response = openai.ChatCompletion.create(
-            model=gpt_mode,
-            temperature=temperature,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Kamu akan mengetikkan sesuatu pada komputer user. Jadi, pastikan jawaban kamu sangat singkat dan tidak perlu penjelasan apa pun. Misal, user ingin kamu menuliskan topi dalam bahasa inggris, maka kamu akan menjawab 'hat' tanpa ada tambahan jawaban lain."
-                },
-                {
-                    "role": "user",
-                    "content": text
-                }
-            ],
-            max_tokens=5,
-        )
-        answer = response['choices'][0]['message']['content']
+    response = openai.ChatCompletion.create(
+        model=gpt_mode,
+        messages=[
+            {
+                "role": "system",
+                "content": templates_obj[template]
+            },
+            {
+                "role": "user",
+                "content": text
+            }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response['choices'][0]['message']['content']
+
+
+def evaluate_command(text):
+    if (translate("terima kasih") in text.lower() or translate("terima kasih", "en-US") in text.lower()):
+        return False
+
+    if (translate("tulis") in text or translate("tulis", "en-US") in text):
+        answer = ask_gpt(text, "write")
         print(answer)
         type_text(answer)
-        text_to_audio(answer, output_filename)
-        play_audio(output_filename)
+        play_audio_from_text(answer)
+        # play_audio_from_text_google(answer)
     else:
-        response = openai.ChatCompletion.create(
-            model=gpt_mode,
-            temperature=temperature,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Kamu adalah seorang yang sangat pintar dari semua pelajaran. Jawablah semua pertanyaan dari user. Tidak perlu menjelaskan secara spesifik dan jawab secukupnya. Tidak perlu selalu mengucapkan terima kasih, tapi tanyakan apakah user ingin bertanya lagi. Jika user tidak ingin bertanya lagi, baru ucapkan 'terima kasih ya!'"
-                },
-                {
-                    "role": "user",
-                    "content": text
-                }
-            ],
-            max_tokens=5,
-        )
-        answer = response['choices'][0]['message']['content']
+        answer = ask_gpt(text)
         print(answer)
-        text_to_audio(answer, output_filename)
-        play_audio(output_filename)
-        if ("terima kasih" in answer.lower()):
+        play_audio_from_text(answer)
+        # play_audio_from_text_google(answer)
+        if (translate("terima kasih") in answer.lower() or translate("terima kasih", "en-US") in answer.lower()):
             return False
     return True
 
@@ -104,47 +167,19 @@ def save_audio(frames, filename):
         wf.writeframes(b''.join(frames))
 
 
-# def transcribe_audio_to_text_using_gpt(audio_file_name, temperature=0):
-#     system_prompt = "Kamu adalah Joni, seorang editor untuk perusahaan ZyntriQix. Tugas kamu adalah untuk memperbaiki kesalahan kata yang diajukan oleh user. Tidak perlu menjawab pertanyaan dari user dan tidak perlu mengulangi kata yang disebutkan oleh user. Pastikan nama dari produk-produk berikut ini sesuai: ZyntriQix, Digique Plus, CynapseFive, VortiQore V8, EchoNix Array, OrbitalLink Seven, DigiFractal Matrix, PULSE, RAPT, B.R.I.C.K., Q.U.A.R.T.Z., F.L.I.N.T. Hanya tambah tanda baca yang perlu seperti titik, koma, kapital, dan jika terdapat konteks tambahan."
-#     try:
-#         audio_file = open(audio_file_name, "rb")
-#         transcript = openai.Audio.transcribe("whisper-1", audio_file)
-#         response = openai.ChatCompletion.create(
-#             model="gpt-3.5-turbo",
-#             temperature=temperature,
-#             messages=[
-#                 {
-#                     "role": "system",
-#                     "content": system_prompt
-#                 },
-#                 {
-#                     "role": "user",
-#                     "content": transcript["text"]
-#                 }
-#             ]
-#         )
-#         transcript = response['choices'][0]['message']['content']
-#         print("Transription: ", transcript)
-#         return transcript.lower()
-#     except Exception as e:
-#         print(e)
-#         print("Could not understand audio")
-#         return ""
-
-
-def transcribe_audio_to_text(filename):
+def transcribe_audio_to_text(filename, lang=LANGUAGE):
     r = sr.Recognizer()
-    # use "test.wav" as the audio source
     with sr.WavFile(filename) as source:
         # extract audio data from the file
         audio = r.record(source)
 
     try:
-        words = r.recognize_google(audio, language="id-ID").lower()
         # recognize speech using Google Speech Recognition
+        words = r.recognize_google(audio, language=lang).lower()
         print("Transcription: " + words)
         return words
-    except:                                 # speech is unintelligible
+    except:
+        # speech is unintelligible
         print("Could not understand audio")
         return ""
 
@@ -173,7 +208,7 @@ def main():
             data = stream.read(CHUNK)
             volume = calculate_volume([int.from_bytes(
                 data[i:i+2], byteorder='little', signed=True) for i in range(0, len(data), 2)])
-
+            # print("." * round(volume / 40))
             if volume > THRESHOLD:
                 if not is_recording:
                     print("Listening...")
@@ -194,25 +229,26 @@ def main():
                 if (not is_eval_mode and silence_timer and time.time() - silence_timer > SILENCE_HOTWORD_INTERVAL) or (is_eval_mode and silence_timer and time.time() - silence_timer > SILENCE_EVAL_INTERVAL):
                     if len(speak_frames) > 0:
                         print("Evaluating...")
-                        filename = "recorded_audio.wav"
-                        save_audio(frames, filename)
-                        text = transcribe_audio_to_text(filename)
+                        save_audio(frames, USER_VOICE_FILENAME)
+                        text = transcribe_audio_to_text(USER_VOICE_FILENAME)
                         if (is_eval_mode):
                             print("Command: " + text)
                             result = evaluate_command(text)
                             is_eval_mode = result
                         elif (HOTWORD in text):
-                            output_filename = "output.mp3"
-                            answer_text = "Iya, ada apa?"
-                            text_to_audio(answer_text, output_filename)
-                            play_audio(output_filename)
+                            answer_text = "Hi!"
+                            # play_audio_from_text(answer_text)
+                            play_audio_from_text_google(answer_text)
                             print(answer_text)
                             is_eval_mode = True
+                        is_recording = False
                         frames = []
                         speak_frames = []
     except KeyboardInterrupt:
         print("Stopped.")
     finally:
+        # os.remove(AGENT_VOICE_FILENAME)
+        # os.remove(USER_VOICE_FILENAME)
         stream.stop_stream()
         stream.close()
         p.terminate()
